0a4361c361e57608b5b5bf9c4e9960573bc2145e
Mahmoud metawea <mahmoudmetawea161@gmail.com>
2022-10-11 07:35:55 +0200

    cart component not complete

---
+++ E-Commerce/src/app/cart/cartcompo/cartcompo.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cartcompo',
+  templateUrl: './cartcompo.component.html',
+  styleUrls: ['./cartcompo.component.scss']
+})
+export class CartcompoComponent implements OnInit {
+  getcartarr:any[]=[]
+amount:number=1
+total:any=0
+totalnumber:any=0
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getcardetails()
+
+  } 
+
+  
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+  getcardetails(){
+    
+    this.getcartarr= JSON.parse(localStorage.getItem("cart")!)
+    
+    console.log( this.getcartarr)
+    this.getcardtotlal()
+  }
+
+
+////////////////////////////////////////////////////////////////////////////////
+  deleteproduct(i:number){
+    this.getcardtotlal()
+  this.getcartarr.splice(i,1)
+  localStorage.setItem("cart",JSON.stringify(this.getcartarr))
+  
+  }
+  getcardtotlal(){
+this.totalnumber=0
+
+for (let x in this.getcartarr){
+  this.totalnumber+= this.getcartarr[x].price * this.amount
+  localStorage.setItem("cart",JSON.stringify(this.getcartarr))
+
+
+}
+ 
+  }
+////////////////////////////////////////////////////
+clearproduct(){
+  this.getcartarr=[]
+  localStorage.setItem("cart",JSON.stringify(this.getcartarr))
+ this. getcardtotlal()
+}
+
+}
